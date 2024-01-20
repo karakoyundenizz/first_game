485980237e7183c3e329490bcfa268556c71e847
karakoyundenizz <157227908+karakoyundenizz@users.noreply.github.com>
2024-01-20 19:48:25 +0300

    Add files via upload

---
+++ yeni_oyun.py
@@ -0,0 +1,24 @@
+import turtle
+
+screen = turtle.Screen((800,600)) # açılacak ekranı yapar
+screen.title("pong oyunu by deniz") # başlık
+screen.bgcolor("black") # background color için kullanırlır
+screen.tracer(0)  # bunu yapmazsan oyun çok yavaş oluur
+
+# oyuncular ve top
+oyuncu1 = turtle.Turtle()
+oyunc
+
+
+oyuncu2 = turtle.Turtle()
+
+
+
+
+
+#game loop
+running = True
+
+while running :
+    screen.update()
+    
